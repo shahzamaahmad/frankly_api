5831d0d2cd9a49ffca0ce98aa42b53f9cff3f9e9
StoreKeeper <dev.shahzama@gmail.com>
2025-10-19 03:48:18 +0400

    problem

--- src/routes/delivery.js
+++ src/routes/delivery.js
@@ -54,8 +54,8 @@ router.post('/', checkPermission('addDeliveries'), (req, res, next) => {
     const DeliveryModel = require('../models/delivery');
     
     const now = new Date();
-    const dd = String(now.getDate()).padLeft(2, '0');
-    const mm = String(now.getMonth() + 1).padLeft(2, '0');
+    const dd = String(now.getDate()).padStart(2, '0');
+    const mm = String(now.getMonth() + 1).padStart(2, '0');
     const yyyy = now.getFullYear();
     const dateStr = `${dd}${mm}${yyyy}`;
     const todayPrefix = `DEL-${dateStr}-`;
