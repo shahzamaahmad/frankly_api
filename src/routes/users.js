b7aa8de647cceff93566dd7bcd6858ed254c6ecd
StoreKeeper <dev.shahzama@gmail.com>
2025-10-23 12:43:20 +0400

    barcode

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,7 +7,6 @@ router.get('/', checkPermission(), async (req, res) => {
   try {
     const users = await User.find()
       .select('-password')
-      .populate('assets.item', 'name sku')
       .lean();
     res.json(users);
   } catch (err) {
@@ -19,8 +18,7 @@ router.get('/', checkPermission(), async (req, res) => {
 router.get('/:id', checkPermission(), async (req, res) => {
   try {
     const user = await User.findById(req.params.id)
-      .select('-password')
-      .populate('assets.item', 'name sku');
+      .select('-password');
     if (!user) return res.status(404).json({ message: 'User not found' });
     res.json(user);
   } catch (err) {
